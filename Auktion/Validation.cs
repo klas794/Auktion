4135b290ab02674be95cb58e22fa63cad6c8d7fc
Emil Ekman <Tangooe@gmail.com>
2016-11-15 11:49:13 +0100

    Frontend
    Frontend

---
+++ Auktion/Validation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Auktion
+{
+    public static class Validation
+    {
+        public static bool Text (string text)
+        {
+            text = text.Trim();
+            return Regex.IsMatch(text, @"^[\p{L}\s]+$");
+        }
+        public static bool Numbers(string numbers)
+        {
+            numbers = numbers.Trim();
+            return Regex.IsMatch(numbers, @"^[\p{N}\s]+$");
+        }
+        public static bool TextAndNumbers (string text)
+        {
+            text = text.Trim();
+            return Regex.IsMatch(text, @"^[\p{L}\p{N}\s]+$");
+        }
+    }
+}
