7db3f2ca037896391beef05a648ef79ee40f83e0
KarbelIlias <Charbel.ilias@gmail.com>
2016-11-15 12:49:00 +0100

    update

--- Auktion/DML-9.sql
+++ Auktion/DML-9.sql
@@ -1,4 +1,6 @@
-﻿CREATE PROCEDURE GetMonthlyRevenue AS SELECT SUM(SellPrice) FROM (
+﻿CREATE PROCEDURE GetMonthlyRevenue
+AS
+SELECT SUM(SellPrice) FROM (
 SELECT FORMAT(Auction.EndDate, 'yyyy-MM') AS Month, MAX(Bids.Price) AS SellPrice FROM Auction
 	INNER JOIN Bids ON Bids.AuctionId = Auction.Id
 	GROUP BY Auction.Id, FORMAT(Auction.EndDate, 'yyyy-MM'), Auction.EndDate
