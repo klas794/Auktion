b8bc518e01c1cebdcc511de12fb3ecb4c811531c
Emil Ekman <Tangooe@gmail.com>
2016-11-15 13:25:53 +0100

    ADo

---
+++ Auktion/Models/Auction.cs
@@ -0,0 +1,41 @@
+namespace Auktion
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Auction")]
+    public partial class Auction
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Auction()
+        {
+            AuctionHistory = new HashSet<AuctionHistory>();
+            Bids = new HashSet<Bids>();
+        }
+
+        public int Id { get; set; }
+
+        public int? ProductId { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime Startdate { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime Enddate { get; set; }
+
+        public decimal Startprice { get; set; }
+
+        public int BuyNow { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<AuctionHistory> AuctionHistory { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Bids> Bids { get; set; }
+    }
+}
