6c432aaa4f725e7ee25986e42e770ce733fdc885
Emil Ekman <Tangooe@gmail.com>
2016-11-17 09:27:11 +0100

    All CRUD Done

--- Auktion/Controllers/SupplierController.cs
+++ Auktion/Controllers/SupplierController.cs
@@ -1,10 +1,7 @@
 ﻿using Auktion.Models;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Auktion.Controllers
 {
@@ -17,25 +14,59 @@ namespace Auktion.Controllers
             _auctionModel = new AuctionModel();
         }
 
-        public List<ValidationResult> Create(Auction auction)
+        public List<ValidationResult> Create(Supplier supplier)
         {
-            var context = new ValidationContext(auction, null, null);
-            var result = new List<ValidationResult>();
-            var valid = Validator.TryValidateObject(auction, context, result, true);
+            var validation = Validation.DbValidate(supplier);
 
-            if (valid)
+            if (validation.Item1)
             {
-                _auctionModel.Auction.Add(auction);
+                _auctionModel.Supplier.Add(supplier);
                 _auctionModel.SaveChanges();
             }
 
-            return result;
+            return validation.Item2;
         }
-
         public List<Supplier> Read()
         {
             var suppliers = _auctionModel.Supplier.ToList();
             return suppliers;
+        }
+        public List<ValidationResult> Update(Supplier supplier)
+        {
+            var updateSupplier = _auctionModel.Supplier.Find(supplier.Id);
+
+            updateSupplier.Firstname = supplier.Firstname;
+            updateSupplier.Lastname = supplier.Lastname;
+            updateSupplier.Commission = supplier.Commission;
+            updateSupplier.Email = supplier.Email;
+            updateSupplier.Phone = supplier.Phone;
+            updateSupplier.Address.Street = supplier.Address.Street;
+            updateSupplier.Address.Zip = supplier.Address.Zip;
+            updateSupplier.Address.City = supplier.Address.City;
+            updateSupplier.Address.Country = supplier.Address.Country;
+
+            var validation = Validation.DbValidate(updateSupplier);
+
+            if (validation.Item1)
+            {
+                _auctionModel.SaveChanges();
+            }
+
+            return validation.Item2;
+        }
+        public bool Delete(Supplier supplier)
+        {
+            try
+            {
+                _auctionModel.Supplier.Remove(supplier);
+                _auctionModel.SaveChanges();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
