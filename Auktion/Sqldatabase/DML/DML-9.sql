9d16aee30a94fbf805f0522e9294695190023be7
Klas <klas793@gmail.com>
2016-11-17 14:36:35 +0100

    fix

--- Auktion/Sqldatabase/DML/DML-9.sql
+++ Auktion/Sqldatabase/DML/DML-9.sql
@@ -3,10 +3,12 @@
 CREATE VIEW [Provision per month]
 AS
 SELECT Month, SUM(Commission) AS Commission FROM (
-SELECT FORMAT(Auction.EndDate, 'yyyy-MM') AS Month, MAX(Bids.Price) * Supplier.Commission AS Commission 
+SELECT  FORMAT(Auction.EndDate, 'yyyy-MM') AS Month, 
+		MAX(Bids.Price) * Supplier.Commission AS Commission 
 	FROM Auction
 	INNER JOIN Bids ON Bids.AuctionId = Auction.Id
-	INNER JOIN Supplier ON Supplier.Id = Bids.BidderId
+	INNER JOIN Product ON Product.Id = Auction.ProductId 
+	INNER JOIN Supplier ON Supplier.Id = Product.SupplyId 
 	GROUP BY Auction.Id, FORMAT(Auction.EndDate, 'yyyy-MM'), Auction.EndDate, Supplier.Commission
 	HAVING Auction.EndDate <= GETDATE()
 	) a
