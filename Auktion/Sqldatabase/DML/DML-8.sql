9c61054a16997d5695f867340f01568cc0f1785b
Klas <klas793@gmail.com>
2016-11-19 18:32:00 +0100

    fix

--- Auktion/Sqldatabase/DML/DML-8.sql
+++ Auktion/Sqldatabase/DML/DML-8.sql
@@ -2,5 +2,9 @@
 CREATE VIEW Buyers
 AS
 SELECT Bidder.Id, SUM(h.FinalBid) AS BuysTotal FROM Bidder
-	INNER JOIN AuctionHistory h ON Bidder.Id = h.FinalBidderId
+	INNER JOIN AuctionHistory h ON Bidder.Id = h.BidderId
 	GROUP BY Bidder.Id
+
+GO
+
+SELECT * FROM Buyers
